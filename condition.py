b5aa61a351914d27d5050b4fd058f0c6e71786be
Shiraz Ali <32427100+asl0007@users.noreply.github.com>
2019-01-06 00:33:34 +0530

    Control Flow
    >>> from condition import *
    >>> condition()
    Traceback (most recent call last):
      File "<stdin>", line 1, in <module>
    TypeError: condition() missing 1 required positional argument: 'x'
    >>> condition(9)
    True
    >>> condition("")
    False
    >>> condition('')
    False
    >>> condition([])
    False
    >>> condition(0)
    False

---
+++ condition.py
@@ -0,0 +1,11 @@
+def condition(x):
+    if x==0:
+        return(False)
+    else:
+        if x=="":
+            return(False)
+        else:
+            if x==[]:
+                return(False)
+            else:
+                return(True)
