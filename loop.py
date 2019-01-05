1e2efd9fae71599ac44c9df1e0244d9940654264
Shiraz Ali <32427100+asl0007@users.noreply.github.com>
2019-01-06 01:21:16 +0530

    Update loop.py

--- loop.py
+++ loop.py
@@ -1,6 +1,36 @@
+#////Factors using for loop////
+
 def factors(n):
     flist=[]
     for i in range(1,n+1):
         if n%i==0:
             flist = flist + [i]
     return(flist)
+
+<!--
+#>>> from loop import *
+#>>> factors(98)
+#[1, 2, 7, 14, 49, 98]
+#>>> factors(10)
+#[1, 2, 5, 10]
+-->
+
+
+#////Factors using while loop in reverse order////
+
+def factors(n):
+    flist=[]
+    i=n
+    while i>0:
+        if n%i==0:
+            flist = flist + [i]
+        i=i-1
+    return(flist)
+
+<!--
+#>>> from loop import *
+#>>> factors(98)
+#[98, 49, 14, 7, 2, 1]
+#>>> factors(10)
+#[10, 5, 2, 1]
+-->
