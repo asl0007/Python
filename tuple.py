95b3b491b5faf4593d47fe0abd5e30285f73adef
Shiraz Ali <32427100+asl0007@users.noreply.github.com>
2021-01-08 19:52:05 +0530

    Added numpy.zeros and numpy.ones
    Task
    
    You are given the shape of the array in the form of space-separated integers, each integer representing the size of different dimensions, your task is to print an array of the given shape and integer type using the tools numpy.zeros and numpy.ones.
    
    Input Format
    
    A single line containing the space-separated integers.
    
    Constraints
    1<= each integer <= 3
    
    Output Format
    
    First, print the array using the numpy.zeros tool and then print the array with the numpy.ones tool. 
    
    Sample Input 0
    
    3 3 3
    
    Sample Output 0
    
    [[[0 0 0]
      [0 0 0]
      [0 0 0]]
    
     [[0 0 0]
      [0 0 0]
      [0 0 0]]
    
     [[0 0 0]
      [0 0 0]
      [0 0 0]]]
    [[[1 1 1]
      [1 1 1]
      [1 1 1]]
    
     [[1 1 1]
      [1 1 1]
      [1 1 1]]
    
     [[1 1 1]
      [1 1 1]
      [1 1 1]]]
    
    Explanation 0
    
    Print the array built using numpy.zeros and numpy.ones tools and you get the result as shown.

--- tuple.py
+++ tuple.py
@@ -5,3 +5,11 @@ if __name__ == '__main__':
     integer_list = map(int, input().split())
     t = tuple(integer_list)
     print(hash(t))
+
+# https://www.hackerrank.com/challenges/np-zeros-and-ones/problem?h_r=next-challenge&h_v=zen
+
+import numpy
+
+n = tuple(map(int, input().split()))
+print(numpy.zeros(n, dtype=numpy.int))
+print(numpy.ones(n, dtype=numpy.int))
